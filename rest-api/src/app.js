5faf165ddd2d2f811a86a503e40692e602858d55
JeanRibac <jeanribac95@gmail.com>
2021-03-27 22:53:25 +0200

    moved router into its separate file

--- rest-api/src/app.js
+++ rest-api/src/app.js
@@ -1,30 +1,8 @@
 const http = require('http');
 
-const { getCarList, getCar, postCar, putCar, deleteCar } = require('./controller');
+const { router } = require('./router');
 
-const { HTTP_METHODS } = require('./util')
-
-const api = '/api'
-
-const server = http.createServer((req, res) => {
-  if (req.url === `${api}/cars` && req.method === HTTP_METHODS.GET) {
-    getCarList(req, res);
-  } else if (req.url.match(/\/api\/car\/\w+/) && req.method === HTTP_METHODS.GET) {
-    const [, , , id] = req.url.split('/');
-    getCar(req, res, id);
-  } else if (req.url === `${api}/cars` && req.method === HTTP_METHODS.POST) {
-    postCar(req, res);
-  } else if (req.url.match(/\/api\/car\/\w+/) && req.method === HTTP_METHODS.PUT) {
-    const [, , , id] = req.url.split('/');
-    putCar(req, res, id)
-  } else if (req.url.match(/\/api\/car\/\w+/) && req.method === HTTP_METHODS.DELETE) {
-    const [, , , id] = req.url.split('/')
-    deleteCar(req, res, id)
-  } else {
-    res.writeHead(404, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ message: 'Route not found' }));
-  }
-})
+const server = http.createServer(router);
 
 const PORT = 3000;
 
---
+++ rest-api/src/router.js
@@ -0,0 +1,45 @@
+const { getCarList, getCar, postCar, putCar, deleteCar } = require('./controller');
+const { HTTP_METHODS: { GET, POST, PUT, DELETE }, return40X } = require('./util')
+
+const router = (req, res) => {
+  const { url, method } = req;
+  const api = '/api'
+  const carRegex = RegExp(`/\/${api}\/car\/\w+/`);
+
+  const dict = {
+    [GET]: () => {
+      if (url === `${api}/cars`) {
+        return getCarList(req, res)
+      }
+      if (url.match(carRegex)) {
+        const [, , , id] = url.split('/');
+        return getCar(req, res, id);
+      }
+    },
+    [POST]: () => {
+      if (url === `${api}/cars`) {
+        return postCar(req, res);
+      }
+    },
+    [PUT]: () => {
+      if (url.match(carRegex)) {
+        const [, , , id] = url.split('/');
+        return putCar(req, res, id)
+      }
+    },
+    [DELETE]: () => {
+      if (url.match(carRegex)) {
+        const [, , , id] = url.split('/')
+        return deleteCar(req, res, id)
+      }
+    },
+  }
+
+  if (dict[method] === undefined) {
+    return return40X(res, 404);
+  }
+
+  return dict[method]();
+}
+
+module.exports = { router }
